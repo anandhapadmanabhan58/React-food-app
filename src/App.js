1c23d5eb65fa3693c55c20e01df529608d09d2fe
Anandha Padmanabhan K.U <93422905+anandhapadmanabhan58@users.noreply.github.com>
2022-12-10 02:52:21 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-const App = () => {
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const shoWcardHandler = () => {
+  const showCartHandler = () => {
     setCartIsShown(true);
   };
+
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
 
   return (
-    <div>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={shoWcardHandler} />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
-};
+}
 
 export default App;
